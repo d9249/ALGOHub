d27bcb0445e125a0bcb32db9fced5dde7abcd377
iDeal <60354713+d9249@users.noreply.github.com>
2024-01-10 18:35:35 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,5 @@
+def solution(my_string, overwrite_string, s):
+    my_string = list(my_string)
+    my_string[s:s+len(overwrite_string)] = overwrite_string
+    my_string = ''.join(my_string)
+    return my_string
