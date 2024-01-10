2f1cde446368d080592285afb39bac872417972b
iDeal <60354713+d9249@users.noreply.github.com>
2024-01-10 20:53:44 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, is_prefix):
+    answer = []
+    for i in range(len(my_string)):
+        answer.append(my_string[:i+1])
+    if is_prefix in answer:
+        return 1
+    else:
+        return 0
+    
