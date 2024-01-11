ee6a6f592c27a573cfb7eb7ad7e538e6415e062e
iDeal <60354713+d9249@users.noreply.github.com>
2024-01-11 21:28:37 +0900

    [level Lv.0] Title: 배열 조각하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181893. 배열 조각하기/배열 조각하기.py
@@ -0,0 +1,8 @@
+def solution(arr, query):
+    answer = arr
+    for i, query in enumerate(query):
+        if i%2 == 0:
+            answer = answer[:query+1]
+        else:
+            answer = answer[query:]
+    return answer
