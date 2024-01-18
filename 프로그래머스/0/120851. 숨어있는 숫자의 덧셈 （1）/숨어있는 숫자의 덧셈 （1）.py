7a70e07d5884918a7494c19baeeafa13b3416ccf
iDeal <60354713+d9249@users.noreply.github.com>
2024-01-18 16:52:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = []
+    for i in range(len(my_string)):
+        if my_string[i].isdigit() == True:
+            answer.append(my_string[i])
+    answer = list(map(int, answer))
+    return sum(answer)
